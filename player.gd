0a5fa53c40a1f340e557a37627224a4a7ba4735a
Benjamin Mahr <ben.mahr@gmail.com>
2026-01-03 20:55:06 +0100

    player can un through the level

--- player.gd
+++ player.gd
@@ -1,22 +1,18 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+@export var SPEED = 1
 
 
-func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var horizontal_direction := Input.get_axis("ui_left", "ui_right")
-	if horizontal_direction:
-		velocity.x = horizontal_direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	var vertical_direction := Input.get_axis("ui_up", "ui_down")
-	if vertical_direction:
-		velocity.y = vertical_direction * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+func get_input():
+	look_at(get_global_mouse_position())
+	rotation += PI/2
+	var forward = transform.y.normalized() * -Input.get_axis("move_down", "move_up")
+	var strafe = transform.x.normalized() * Input.get_axis("move_left", "move_right")
+	velocity = (forward + strafe).normalized() * SPEED
 
+
+func _physics_process(delta):
+	get_input()
 	move_and_slide()
+	
