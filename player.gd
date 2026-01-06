e4c39427ba5fdc64cb6fcd83dc9d79175c8a4d74
Benjamin Mahr <ben.mahr@gmail.com>
2026-01-06 06:32:48 +0100

    laser scene appearing on global scene

--- player.gd
+++ player.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 
 
 var is_dead: bool = false
-
+var laser_scene: PackedScene
 
 func get_input():
 	look_at(get_global_mouse_position())
@@ -13,6 +13,9 @@ func get_input():
 	var forward = transform.y.normalized() * -Input.get_axis("move_down", "move_up")
 	var strafe = transform.x.normalized() * Input.get_axis("move_left", "move_right")
 	velocity = (forward + strafe).normalized() * SPEED
+	
+	if (Input.is_action_pressed("shoot")):
+		fire()
 
 
 func _physics_process(delta):
@@ -21,6 +24,7 @@ func _physics_process(delta):
 
 
 func _ready():
+	laser_scene = preload("res://laser.tscn")
 	add_to_group("player")
 
 
@@ -29,3 +33,10 @@ func die() -> void:
 		return
 	is_dead = true
 	get_tree().reload_current_scene()
+
+
+func fire() -> void:
+	var laser = laser_scene.instantiate()
+	laser.global_position = global_position
+	laser.rotation = rotation
+	get_parent().add_child(laser)
