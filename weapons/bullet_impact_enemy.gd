c8e5e5a53fa453b55e242c467b146b413b4a5d95
Benjamin Mahr <ben.mahr@gmail.com>
2026-01-31 18:19:50 +0100

    adding wall and enemy hits

---
+++ weapons/bullet_impact_enemy.gd
@@ -0,0 +1,11 @@
+extends GPUParticles2D
+
+func _ready() -> void:
+	var image = Image.create(2, 2, false, Image.FORMAT_RGBA8)
+	image.fill(Color(1.0, 1.0, 1.0, 1.0))
+	texture = ImageTexture.create_from_image(image)
+	
+	emitting = true
+
+func _on_finished() -> void:
+	queue_free()
