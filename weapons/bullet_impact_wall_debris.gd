7482d469f5a5f1c9b438bb0d65de02ea87d85299
Benjamin Mahr <ben.mahr@gmail.com>
2026-02-07 10:13:28 +0100

    flash ligt, muzzle flash light, ambient light and shadows added

--- weapons/bullet_impact_wall_debris.gd
+++ weapons/bullet_impact_wall_debris.gd
@@ -1,10 +1,15 @@
 extends GPUParticles2D
 
+@onready var light: PointLight2D = $PointLight2D
+
 func _ready() -> void:
 	var image = Image.create(2, 2, false, Image.FORMAT_RGBA8)
 	image.fill(Color(1.0, 1.0, 1.0, 1.0))
 	texture = ImageTexture.create_from_image(image)
 	process_material = process_material.duplicate()
+	
+	var tween = create_tween()
+	tween.tween_property(light, "energy", 0, lifetime).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
 	
 	emitting = true
 
